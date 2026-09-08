f8e92d42fea87b72f71c7a3ce17b5f8514cc02e1
Makinde Kelvin <kelvinmakinde2@gmail.com>
2026-09-08 18:50:01 +0100

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,10 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import { AppContextProvider } from "@/context/AppContext";
 import { Toaster } from "react-hot-toast";
 
-const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
+const outfit = Outfit({ subsets: ["latin"], weight: ["300", "400", "500"] });
 
 export const metadata = {
   title: "QuickCart - GreatStack",
@@ -12,13 +13,26 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
       <html lang="en">
-        <body className={`${outfit.className} antialiased text-gray-700`} >
+        <body className={`${outfit.className} antialiased text-gray-700`}>
           <Toaster />
-          <AppContextProvider>
-            {children}
-          </AppContextProvider>
+          {/* <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header> */}
+          <AppContextProvider>{children}</AppContextProvider>
         </body>
       </html>
+    </ClerkProvider>
   );
 }
---
+++ config/inngest.ts
@@ -0,0 +1,71 @@
+// src/inngest/client.ts
+
+import { Inngest } from "inngest";
+import dbConnect from "./db";
+import User from "@/models/User";
+
+export const inngest = new Inngest({
+  id: "quickcart-next",
+});
+
+// Inngest function to save user data to database
+export const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+    triggers: {
+      event: "clerk/user.created",
+    },
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: `${first_name} ${last_name}`,
+      imageUrl: image_url,
+    };
+
+    await dbConnect();
+    await User.create(userData);
+  },
+);
+
+// Inngest function to update user data to database
+export const syncUserUpdate = inngest.createFunction(
+  {
+    id: "user-update-from-clerk",
+    triggers: {
+      event: "clerk/user.updated",
+    },
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: `${first_name} ${last_name}`,
+      imageUrl: image_url,
+    };
+
+    await dbConnect();
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
+
+// Inngest function to delete user data from database
+export const syncUserDeletion = inngest.createFunction(
+  {
+    id: "user-deletion-from-clerk",
+    triggers: {
+      event: "clerk/user.deleted",
+    },
+  },
+  async ({ event }) => {
+    const { id } = event.data;
+
+    await dbConnect();
+    await User.findByIdAndDelete(id);
+  }
+);
